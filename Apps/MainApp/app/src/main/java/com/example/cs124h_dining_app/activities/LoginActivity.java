5285e514c654c7380d4ce9a993fd51495df4085c
nyssaaftab <nyssa.aftab@gmail.com>
2023-12-02 19:43:56 -0600

    google sign in

--- Apps/MainApp/app/src/main/java/com/example/cs124h_dining_app/activities/LoginActivity.java
+++ Apps/MainApp/app/src/main/java/com/example/cs124h_dining_app/activities/LoginActivity.java
@@ -3,12 +3,15 @@ package com.example.cs124h_dining_app.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cs124h_dining_app.R;
@@ -16,14 +19,37 @@ import com.example.cs124h_dining_app.Time2Eat;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class LoginActivity extends AppCompatActivity {
     TextInputEditText editTextEmail, editTextPassword;
     Button buttonLogin;
+
     FirebaseAuth mAuth;
     Time2Eat appState;
     ProgressBar progressBar;
     TextView textView;
+// google sign in stuff
+    SignInButton googleAuth;
+    FirebaseDatabase database;
+
+    GoogleSignInClient mGoogleSignInClient;
+    int RC_SIGN_IN = 20;
 
     @Override
     public void onStart() {
@@ -52,6 +78,24 @@ public class LoginActivity extends AppCompatActivity {
             Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
             startActivity(intent);
             finish();
+        });
+
+        googleAuth = findViewById((R.id.btnGoogleAuth));
+
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail().build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this,gso);
+
+        googleAuth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                googleSignIn();
+            }
         });
 
         buttonLogin.setOnClickListener(view -> {
@@ -90,4 +134,64 @@ public class LoginActivity extends AppCompatActivity {
 
         });
     }
+
+    private void googleSignIn() {
+        Intent intent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(intent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode==RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuth(account.getIdToken());
+            }
+            catch (Exception e) {
+                Log.e("GoogleSignIn", "Sign-in failed: " + e.getCause());
+                Toast.makeText(this, "Sign-in failed: " + e.getCause(), Toast.LENGTH_SHORT).show();
+                //Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+
+    private void firebaseAuth(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            HashMap<String, Object> map = new HashMap<>();
+                            map.put("id", user.getUid());
+                            map.put("name", user.getDisplayName());
+                            map.put("profile", user.getPhotoUrl().toString());
+
+                            database.getReference().child("users").child(user.getUid()).setValue(map);
+
+                            //TextView signInStatus = findViewById(R.id.status_textview);
+                            //signInStatus.setText("Signed in");
+
+                            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                            startActivity(intent);
+
+                        }
+                        else {
+                            Toast.makeText(LoginActivity.this, "something went wrong", Toast.LENGTH_SHORT).show();
+
+                        }
+
+
+                    }
+                });
+
+    }
+
 }
